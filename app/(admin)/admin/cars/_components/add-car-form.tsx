44086963fc5352b1a0842c705ef3e5075aabd085
Aryal <roozanaryal8@gmail.com>
2025-08-27 19:07:16 +0545

    packet installation

--- app/(admin)/admin/cars/_components/add-car-form.tsx
+++ app/(admin)/admin/cars/_components/add-car-form.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+
+const fuelTypes = ["Petrol", "Diesel", "Electric", "Hybrid"];
+const transmissionTypes = ["Manual", "Automatic"];
+const bodyTypes = [
+  "SUV",
+  "Sedan",
+  "Hatchback",
+  "Coupe",
+  "Convertible",
+  "Wagon",
+  "Pickup",
+];
+
+const carStatus = ["Available", "Not Available", "SOLD"];
 
 const AddCarForm = () => {
-  return (
-    <div></div>
-  )
-}
 
-export default AddCarForm
+    const carFormSchema = z.object({
+        make: z.string().min(1,"Make is required")
+        model:z.string().min(1,"Model is required")
+        year:z.string().refine(val)=>{
+            const year = parseInt(val);
+            return(
+                !isNaN(year) && year>=1900 && year <= new Date().getFullYear()+1
+
+            );
+            
+        },"Valid year required"),
+    })
+
+  return <div>
+    Add Car Form
+  </div>;
+};
+
+export default AddCarForm;
